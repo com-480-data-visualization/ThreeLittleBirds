58c95b8743741181c22e131efade43c849c713c2
Ristafan <martin.faehnrich@gmail.com>
2026-04-10 16:31:25 +0200

    First Airplane visual

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@
    on scroll, lazy-image fade-in.
    ========================================================= */
 import './components/primary_vis.js';
+import './components/secondary_vis.js';
 import './components/tertiary_vis.js';
 import './components/sliders.js';
 import './styles/main.css';
